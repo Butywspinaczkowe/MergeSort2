6f47be44c7b792e415937230bb4370735f83ba21
but <buty.wspinaczkowe@gmail.com>
2020-11-28 17:47:04 +0000

    Add project files.

---
+++ MergeSort2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MergeSort2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merge Sort");
+
+            int[] randomArray = Populate(101);
+            Console.WriteLine("Here is a randomly populated array: ");
+            WriteArray(randomArray);
+            Console.WriteLine("\nHere is that array sorted using merge sort:");
+            WriteArray(Sort.MergeSort(randomArray));
+
+            
+
+
+
+            
+
+
+            static int[] Populate(int arrayLength)
+            {
+                int[] outputArray = new int[arrayLength];
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    outputArray[i] = RandomNumGen(101);
+                }
+                return outputArray;
+            }
+
+            static int RandomNumGen(int max)
+            {
+                Random randNum = new Random();
+                return randNum.Next(max);
+            }
+
+            static void WriteArray(int[] inputArray)
+            {
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    Console.Write(inputArray[i] + " ");
+                }
+            }
+
+        }
+    }
+}
---
+++ MergeSort2/Sort.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MergeSort2
+{
+    class Sort
+    {
+        public static int[] MergeSort(int[] inputArray)
+        {
+            if (inputArray.Length <= 1)
+            {
+                return inputArray;
+            }
+
+            int[] leftArray = new int[inputArray.Length / 2];
+            int[] rightArray = new int[inputArray.Length / 2 + (inputArray.Length % 2)];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (i < inputArray.Length / 2)
+                {
+                    leftArray[i] = inputArray[i];
+                }
+                else
+                {
+                    rightArray[i - inputArray.Length / 2] = inputArray[i];
+                }
+            }
+
+            leftArray = MergeSort(leftArray);
+            rightArray = MergeSort(rightArray);
+
+            return Merge(leftArray, rightArray);
+        }
+
+        public static int[] MergeSort(int[] inputArray, int highValue, int lowValue)
+        {
+            //int[] newArray = ArraySlicer(inputArray, highValue, lowValue);
+
+            if (inputArray.Length <= 1)
+            {
+                return inputArray;
+            }
+
+            int[] leftArray = new int[inputArray.Length / 2];
+            int[] rightArray = new int[inputArray.Length / 2 + (inputArray.Length % 2)];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (i < inputArray.Length / 2)
+                {
+                    leftArray[i] = inputArray[i];
+                }
+                else
+                {
+                    rightArray[i - inputArray.Length / 2] = inputArray[i];
+                }
+            }
+
+            leftArray = MergeSort(leftArray);
+            rightArray = MergeSort(rightArray);
+
+            return Merge(leftArray, rightArray);
+        }
+
+        static int[] Merge(int[] leftArray, int[] rightArray)
+        {
+            int[] sortedList = new int[leftArray.Length + rightArray.Length];
+            int i = 0;
+            while (leftArray.Length > 0 && rightArray.Length > 0)
+            {
+                if (leftArray[0] <= rightArray[0])
+                {
+                    sortedList[i] = leftArray[0];
+                    ResizeArray(ref leftArray);
+                }
+                else
+                {
+                    sortedList[i] = rightArray[0];
+                    ResizeArray(ref rightArray);
+                }
+                i++;
+            }
+            while (leftArray.Length > 0)
+            {
+                sortedList[i] = leftArray[0];
+                ResizeArray(ref leftArray);
+                i++;
+            }
+            while (rightArray.Length > 0)
+            {
+                sortedList[i] = rightArray[0];
+                ResizeArray(ref rightArray);
+                i++;
+            }
+            return sortedList;
+        }
+
+        static int[] ResizeArray(ref int[] array)
+        {
+            int[] temp = new int[array.Length - 1];
+
+            for (int i = array.Length - 1; i >= 1; i--)
+                temp[i - 1] = array[i];
+
+            array = temp;
+            return array;
+        }
+
+       /* static int[] ArraySlicer(int[] inputArray, int highValue, int lowValue)
+        {
+            for (int i = lowValue; i < highValue; i++)
+            {
+                return 
+            }
+        }*/
+    }
+}
